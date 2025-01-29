87a635bee2c55daa66afc07d9a24736c014af992
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2025-01-29 20:14:53 +0200

    change sprites to items in 01_Forest

--- Resources/Scripts/Group/Group01_Forest.py
+++ Resources/Scripts/Group/Group01_Forest.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ef644c920282b3b07649f4c3f18eb1acca89b6a075276662886973012f9ca82
-size 1056
+oid sha256:503a8a59edceb738ede579306608d21cfd684ef05a3377636a1dc9a2b4a27e19
+size 1474
