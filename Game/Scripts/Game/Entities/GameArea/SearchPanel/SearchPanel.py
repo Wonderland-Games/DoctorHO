239f351e8e2cb565312f7ed832a3c8a7e7574087
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2025-01-30 19:56:42 +0200

    add Item into SearchPanel

--- Game/Scripts/Game/Entities/GameArea/SearchPanel/SearchPanel.py
+++ Game/Scripts/Game/Entities/GameArea/SearchPanel/SearchPanel.py
@@ -1,5 +1,6 @@
 from Foundation.Initializer import Initializer
 from Foundation.TaskManager import TaskManager
+from Game.Entities.GameArea.SearchPanel.Item import Item
 
 
 MOVIE_PANEL = "Movie2_SearchPanel"
@@ -8,15 +9,16 @@ MOVIE_PANEL = "Movie2_SearchPanel"
 class SearchPanel(Initializer):
     def __init__(self):
         super(SearchPanel, self).__init__()
-        self._game = None
+        self.game = None
         self.root = None
         self.movie_panel = None
         self.tcs = []
 
     def _onInitialize(self, game):
-        self._game = game
+        self.game = game
         self._createRoot()
         self._attachPanel()
+        self._initItems()
         return True
 
     def _onActivate(self):
@@ -42,16 +44,22 @@ class SearchPanel(Initializer):
         self.root.removeFromParent()
         node.addChild(self.root)
 
-    def _attachPanel(self):
-        self.movie_panel = self._game.object.getObject(MOVIE_PANEL)
-        movie_panel_node = self.movie_panel.getEntityNode()
-        self.root.addChild(movie_panel_node)
-
     def getHeight(self):
-        movie_panel = self._game.object.getObject(MOVIE_PANEL)
+        movie_panel = self.game.object.getObject(MOVIE_PANEL)
         movie_panel_bounds = movie_panel.getCompositionBounds()
         movie_panel_height = Utils.getBoundingBoxHeight(movie_panel_bounds)
         return movie_panel_height
+
+    def _attachPanel(self):
+        self.movie_panel = self.game.object.getObject(MOVIE_PANEL)
+        movie_panel_node = self.movie_panel.getEntityNode()
+        self.root.addChild(movie_panel_node)
+
+    def _initItems(self):
+        for item_obj in self.game.items:
+            item = Item()
+            item.onInitialize(item_obj)
+            item.attachTo(self.root)
 
     def _createTaskChain(self, name, **params):
         tc = TaskManager.createTaskChain(Name=self.__class__.__name__+"_"+name, **params)
--- Resources/Scripts/Group/Group01_Forest.py
+++ Resources/Scripts/Group/Group01_Forest.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:503a8a59edceb738ede579306608d21cfd684ef05a3377636a1dc9a2b4a27e19
-size 1474
+oid sha256:3a26ba8b5091137da3cf07ce7fd7b92f83ec94fc6c750c97f0aade3acb5051ca
+size 1478
