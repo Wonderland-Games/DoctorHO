d858b7b6a2eb487ed99367a8aed4ab62df1e9e0a
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2025-02-07 22:15:13 +0200

    add ItemsCounter into SearchPanel

---
+++ Game/Scripts/Game/Entities/GameArea/SearchPanel/ItemsCounter.py
@@ -0,0 +1,56 @@
+from Foundation.Initializer import Initializer
+
+
+TEXT_ID = "ID_SearchPanel_ItemsCounter"
+
+
+class ItemsCounter(Initializer):
+    def __init__(self):
+        super(ItemsCounter, self).__init__()
+        self.items_count = None
+        self.items_amount = None
+        self.node = None
+
+    def _onInitialize(self, items_count, items_amount):
+        self.items_count = items_count
+        self.items_amount = items_amount
+
+        self._createTextNode()
+
+    def _onFinalize(self):
+        self.items_count = None
+        self.items_amount = None
+
+        if self.node is not None:
+            self.node.removeFromParent()
+            Mengine.destroyNode(self.node)
+            self.node = None
+
+    def _createTextNode(self):
+        self.node = Mengine.createNode("TextField")
+        self.node.setName(self.__class__.__name__)
+        self.node.setVerticalCenterAlign()
+        self.node.setHorizontalCenterAlign()
+        self.node.setTextId(TEXT_ID)
+        self.updateTextArgs()
+        self.node.enable()
+
+    def attachTo(self, node):
+        self.node.removeFromParent()
+        node.addChild(self.node)
+
+    def setLocalPosition(self, pos):
+        self.node.setLocalPosition(pos)
+
+    def updateTextArgs(self, value_1=None, value_2=None):
+        if value_1 is None:
+            value_1 = self.items_count
+
+        if value_2 is None:
+            value_2 = self.items_amount
+
+        self.node.setTextFormatArgs(value_1, value_2)
+
+    def incItemsCount(self):
+        self.items_count += 1
+        self.updateTextArgs()
