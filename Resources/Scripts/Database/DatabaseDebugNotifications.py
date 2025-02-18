29f79f9ff40b9926ea668346de1d4df44dd5941e
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2025-02-18 21:57:23 +0200

    add BlockInput and Fade, tried to add PopUps...

--- Resources/Scripts/Database/DatabaseDebugNotifications.py
+++ Resources/Scripts/Database/DatabaseDebugNotifications.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1cbd7e838f471bc439a7b20e4eb3ffd98e84335eb038a7c51b0bcb1cc2101f3c
-size 504
+oid sha256:13e906bcbd972ad0db7018b05709f70afad33a27e328e9335ebc7af725c2e880
+size 551
