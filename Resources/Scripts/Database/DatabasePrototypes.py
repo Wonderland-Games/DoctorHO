d7c5e7e0964b2c9127edc36d9801bfedd9ef27ec
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2025-02-19 20:31:14 +0200

    progress in PopUp

--- Resources/Scripts/Database/DatabasePrototypes.py
+++ Resources/Scripts/Database/DatabasePrototypes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f3552197c0bc9e1b0eb9c60145cb15d521a922d7a641ad52899f86efdb27efd0
+oid sha256:2d03763b60084214f7966439cd5753ed37aa8ef0ed111a8b068b30f8c85fbaaa
 size 1155
