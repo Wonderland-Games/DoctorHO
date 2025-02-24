be8aef3b79b529931e0a9717fe9bcc9b654a92d5
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2025-02-24 21:36:13 +0200

    add Settings button

--- Resources/Scripts/Database/DatabaseIcons.py
+++ Resources/Scripts/Database/DatabaseIcons.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8beb9fba7dfe9b7583e21241af2789cabb5701642c159db0cd1d27e82e93a9ea
-size 1070
+oid sha256:cbf4e1299baadd2a2e1f3016afde8069832173435f50888ea4b206421c3df5bc
+size 1242
--- Resources/Scripts/Group/GroupUIStore.py
+++ Resources/Scripts/Group/GroupUIStore.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e4091529a600f3b9674e93197ef0548ac86a933fe4299bbd0b28933bcea6970e
-size 1403
+oid sha256:aa07365fdeb46d667086bace7f99ce2fd5e935d010750f29aed18f3a9bfcc125
+size 2247
