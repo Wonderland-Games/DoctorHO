0111fd3fa0c0632dda737edf57c09841c4cf5f4a
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2025-02-28 00:07:40 +0200

    finished Settings popup

--- Resources/Scripts/Database/DatabaseIcons.py
+++ Resources/Scripts/Database/DatabaseIcons.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f2548a7fe9cb0553fa78ae44428068d07374b835caef1423641e349b48ead49b
-size 1903
+oid sha256:eac1819e5160df74f2d9d51e5e76c528dc5ea56c2bd79f2f4f23177c3f6cdf70
+size 2416
--- Resources/Scripts/Database/DatabasePrototypes.py
+++ Resources/Scripts/Database/DatabasePrototypes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ebbea0c548f72f5f8122daf3d5fdeccdba617f8d01bf1a6b3ca43f1bd01783e8
-size 2502
+oid sha256:c902ffcfa816b4e1e9aab6957994aa6587945fb79d6aa3ba8a0244d45d1b5394
+size 2501
