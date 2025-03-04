c55011f3d7655920ccf016e9e7719ee9ecd7650a
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2025-03-04 21:27:50 +0200

    add AdvertisingScene

---
+++ Resources/Scripts/Database/DatabaseSceneSlots_Advertising.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8155fa18d54ddfb456a963d39af63751207ac0f3e0fa4e40031d3678fe8e6697
+size 467
---
+++ Resources/Scripts/Group/GroupAdvertising.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3e2a5808c78868afb7c213ec53a0577c4c8a66038a94c2b3098153d410c46fa5
+size 675
