54513bc4faf5cf246c1f9587729414f5cef0157f
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2025-03-04 20:11:02 +0200

    add SystemUI for adjusting prototypes texts environment

---
+++ Game/Scripts/Game/Systems/SystemUI.py
@@ -0,0 +1,11 @@
+from UIKit.Systems.SystemUserInterface import SystemUserInterface as BaseSystem
+
+
+class SystemUI(BaseSystem):
+    def _setTexts(self):
+        Mengine.setTextAlias("Settings_Languages", "$UIButton", "ID_Settings_Languages")
+        Mengine.setTextAlias("Settings_Support", "$UIButton", "ID_Settings_Support")
+        Mengine.setTextAlias("Settings_Credits", "$UIButton", "ID_Settings_Credits")
+
+        Mengine.setTextAlias("LevelLost_Ad", "$UIButton", "ID_LevelLost_Ad")
+        Mengine.setTextAlias("LevelLost_Restart", "$UIButton", "ID_LevelLost_Restart")
