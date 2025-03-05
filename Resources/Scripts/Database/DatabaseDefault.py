1e650b3376641b85a80691a794dbfe6eb189ef86
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2025-03-05 16:07:06 +0200

    change transition logic

--- Resources/Scripts/Database/DatabaseDefault.py
+++ Resources/Scripts/Database/DatabaseDefault.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba4caef2263798b67d738ebd36543340c9221235d32b8f414710b08d38fe493f
-size 169
+oid sha256:65ab31bd7013efab9f87d8293ee01792f0d3b08ef24a63ebe55cbfdd57a62be6
+size 632
--- Resources/Scripts/Group/GroupBlockInput.py
+++ Resources/Scripts/Group/GroupBlockInput.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d70c6f1bafef27df38a889fcba4c8dc942d47c227bcb8c8c5730109c2e6cbc9
-size 593
+oid sha256:9b14fbbfb56f3f8c0cae1cba890eb9de17794be845f205ebe0021802a4327e07
+size 720
