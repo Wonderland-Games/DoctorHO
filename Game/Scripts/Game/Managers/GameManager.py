b597779e702de543b9bdcaff9a9e73c651ded42c
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2025-03-05 19:41:25 +0200

    wip GameManager

--- Game/Scripts/Game/Managers/GameManager.py
+++ Game/Scripts/Game/Managers/GameManager.py
@@ -1,11 +1,51 @@
 from Foundation.Manager import Manager
+from Foundation.DemonManager import DemonManager
 
 
 class GameManager(Manager):
-    @classmethod
-    def _onInitialize(cls, *args):
-        print "INITIALIZING"
+    _current_game_params = None
+
+    # - Manager --------------------------------------------------------------------------------------------------------
 
     @classmethod
-    def _onLoad(cls, dict_save):
-        print "LOADING SAVE"
+    def _onInitialize(cls, *args):
+        pass
+
+    @classmethod
+    def _onFinalize(cls):
+        GameManager._current_game_params = None
+
+    @classmethod
+    def _onSave(cls):
+        save_data = {}
+        return save_data
+
+    @classmethod
+    def _onLoad(cls, saved_data):
+        pass
+
+    # - Game -----------------------------------------------------------------------------------------------------------
+
+    @staticmethod
+    def prepareGame(game_type, level_name):
+        game = DemonManager.getDemon("GameArea")
+
+        game.setGameType(game_type)
+        game.setLevelName(level_name)
+
+        # params_orm = GameManager.getGameLevelParams(game_type, level_name)
+        # GameManager._current_game_params = params_orm
+
+    @staticmethod
+    def endGame():
+        pass
+
+    @staticmethod
+    def getCurrentGameParams():
+        return GameManager._current_game_params
+
+    @staticmethod
+    def getCurrentGameParam(param):
+        game = DemonManager.getDemon("GameArea")
+        game_param = game.getParam(param)
+        return game_param
