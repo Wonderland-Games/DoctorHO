d349ecd35c6a5c2faf0f7167fe827129cfbcc9cf
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2025-03-13 15:08:30 +0200

    add SearchItemsCorners

--- Resources/Scripts/Database/DatabasePrototypes.py
+++ Resources/Scripts/Database/DatabasePrototypes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1303bc15b24cc97e9cd102f7b56a405610ca61fb1969e733299ae45f01588441
-size 3965
+oid sha256:758529f9c87fd708c5d5a2c28a1bcc578965288837cebf403e3f15dd725aef1d
+size 4266
