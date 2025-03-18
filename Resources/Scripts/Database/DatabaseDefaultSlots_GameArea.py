6399722217898ac4d96a0e5aa137af30466ca2e7
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2025-03-18 21:26:00 +0200

    add new level 02_Atlantis

--- Resources/Scripts/Database/DatabaseDefaultSlots_GameArea.py
+++ Resources/Scripts/Database/DatabaseDefaultSlots_GameArea.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b434ee80ebd7dbd9b062a9d5671c3989e0c5102e721adbe81e2bed545e280e48
-size 763
+oid sha256:7eaca4cb307c1cff0fcfbe84ec45a2c2da055d796bd937586e663c0601eaafb0
+size 851
--- Resources/Scripts/Database/DatabaseGroups.py
+++ Resources/Scripts/Database/DatabaseGroups.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:23971b9117b1d29f1d62e9c6c2b87011d3a2633d9ad876b0729baf932c16720c
-size 655
+oid sha256:fa13991eb7591e1b92067dfe842419948243f997afeaec0d40d22494dc0c9228
+size 703
