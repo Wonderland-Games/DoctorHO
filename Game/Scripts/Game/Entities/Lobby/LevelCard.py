e9597536c4ca27d3812ce6a6eac2adf9b685edcf
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2025-03-20 21:32:42 +0200

    add titile alias and texts for LevelCard

--- Game/Scripts/Game/Entities/Lobby/LevelCard.py
+++ Game/Scripts/Game/Entities/Lobby/LevelCard.py
@@ -5,6 +5,8 @@ from Game.Managers.LevelCardManager import LevelCardManager
 
 PROTOTYPE_CARD = "LevelCard"
 SLOT_LEVEL = "Level"
+ALIAS_TITLE = "$LevelCardTitle"
+TEXT_TITLE = "ID_LevelCardTitle"
 
 
 class LevelCard(Initializer):
@@ -13,7 +15,7 @@ class LevelCard(Initializer):
         self.level_name = None
         self.root = None
         self.button = None
-        self.movie_level = None
+        self.level = None
 
     # - Initializer ----------------------------------------------------------------------------------------------------
 
@@ -22,15 +24,17 @@ class LevelCard(Initializer):
         self.level_name = level_name
 
         self._createRoot()
+
         self._setupButton()
         self._setupLevel()
+        self._setupTitle()
 
     def _onFinalize(self):
         super(LevelCard, self)._onFinalize()
 
-        if self.movie_level is not None:
-            self.movie_level.onDestroy()
-            self.movie_level = None
+        if self.level is not None:
+            self.level.onDestroy()
+            self.level = None
 
         if self.button is not None:
             self.button.onDestroy()
@@ -59,20 +63,32 @@ class LevelCard(Initializer):
     def getRoot(self):
         return self.root
 
-    # - Button ---------------------------------------------------------------------------------------------------------
+    # - Setup ----------------------------------------------------------------------------------------------------------
 
     def _setupButton(self):
         self.button = PrototypeManager.generateObjectUniqueOnNode(self.root, PROTOTYPE_CARD, PROTOTYPE_CARD)
         self.button.setEnable(True)
 
+    def _setupLevel(self):
+        self.level = LevelCardManager.generateLevelCard(self.level_name)
+        self.level.setEnable(True)
+
+        level_node = self.level.getEntityNode()
+        self.button.addChildToSlot(level_node, SLOT_LEVEL)
+
+    def _setupTitle(self):
+        env = PROTOTYPE_CARD + "_" + self.level_name
+        title_id = TEXT_TITLE + "_" + self.level_name
+        title_text = Mengine.getTextFromId(title_id)
+
+        self.button.setTextAliasEnvironment(env)
+
+        Mengine.setTextAlias(env, ALIAS_TITLE, TEXT_TITLE)
+        Mengine.setTextAliasArguments(env, ALIAS_TITLE, title_text)
+
+    # - Utils ----------------------------------------------------------------------------------------------------------
+
     def getSize(self):
         button_bounds = self.button.getCompositionBounds()
         button_size = Utils.getBoundingBoxSize(button_bounds)
         return button_size
-
-    def _setupLevel(self):
-        self.movie_level = LevelCardManager.generateLevelCard(self.level_name)
-        self.movie_level.setEnable(True)
-
-        level_node = self.movie_level.getEntityNode()
-        self.button.addChildToSlot(level_node, SLOT_LEVEL)
