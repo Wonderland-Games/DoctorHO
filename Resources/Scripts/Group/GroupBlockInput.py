e8b132b348dbcd902de80671bcef0630761d6e41
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2025-03-20 20:34:52 +0200

    change blockinput hotspot

--- Resources/Scripts/Group/GroupBlockInput.py
+++ Resources/Scripts/Group/GroupBlockInput.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b14fbbfb56f3f8c0cae1cba890eb9de17794be845f205ebe0021802a4327e07
+oid sha256:d1b8f6d139c0d9f61a9f8bcd552f6467d220adc2a4cf0a187d9ffe8ec64ba9f7
 size 720
