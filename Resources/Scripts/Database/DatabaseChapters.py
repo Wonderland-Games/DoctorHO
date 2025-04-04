1bda8e717a8ebc1387e174723f01dbd0d96c11e8
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2025-04-04 18:29:01 +0300

    WIP GameManager dummy data

--- Resources/Scripts/Database/DatabaseChapters.py
+++ Resources/Scripts/Database/DatabaseChapters.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6273f5e4864e7d1acd8f78934cb3d75860a510f162c1ea7ab64f4c10d4644237
-size 994
+oid sha256:ef666d7bb096bf5f0ec21eedf6d0076293f53aae4c10abd438ee8659087d0413
+size 785
