5f89c0e85595ffb850cf74c5967e2aa9b4fc37c9
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2025-04-04 22:10:49 +0300

    WIP set dummy player data

--- Game/Scripts/Game/Managers/GameData.py
+++ Game/Scripts/Game/Managers/GameData.py
@@ -1,28 +1,22 @@
-class LevelData(object):
-    def __init__(self, level_name):
-        self.level_name = level_name
-
-
 class ChapterData(object):
     def __init__(self, chapter_name):
         self.chapter_name = chapter_name
-        self.levels = {}
-        self.current_level = None
+        self.current_levels = []
 
-    def getLevelData(self, level):
-        return self.levels.get(level)
+    def getChapterName(self):
+        return self.chapter_name
 
-    def getCurrentLevelData(self):
-        return self.getLevelData(self.current_level)
+    def getCurrentLevels(self):
+        return self.current_levels
 
 
 class PlayerGameData(object):
     def __init__(self):
-        self.chapters = {}
         self.current_chapter = None
 
-    def getChapterData(self, chapter):
-        return self.chapters.get(chapter)
-
     def getCurrentChapterData(self):
-        return self.getChapterData(self.current_chapter)
+        return self.current_chapter
+
+    def loadData(self, active_chapter_name, active_levels_names):
+        self.current_chapter = ChapterData(active_chapter_name)
+        self.current_chapter.current_levels = active_levels_names
