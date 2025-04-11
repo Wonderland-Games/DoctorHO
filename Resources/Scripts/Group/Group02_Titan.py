0d88e1e61869f52c81c4a5232a2fd61aa9246642
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2025-04-11 16:49:30 +0300

    rework quest items names

--- Resources/Scripts/Group/Group02_Titan.py
+++ Resources/Scripts/Group/Group02_Titan.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:75ff72bf03950e370c29949ab3c0efaa193b33a813a8228563b6f9c698344d3a
-size 4608
+oid sha256:5cebef36a22a2ca740af4a1fbeaf65ee5e200f32d48bc783f024ba8fc311e8b0
+size 4638
