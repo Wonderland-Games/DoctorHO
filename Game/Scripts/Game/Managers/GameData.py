1ed5efb2cfeb85f6a3e75f30bc47abd740b0b081
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2025-04-18 21:19:41 +0300

    WIP QuestPoints implementation + improvements

--- Game/Scripts/Game/Managers/GameData.py
+++ Game/Scripts/Game/Managers/GameData.py
@@ -1,34 +1,52 @@
 class ChapterData(object):
     def __init__(self, chapter_id):
         self.chapter_id = chapter_id
-        self.current_levels_id = []
+        self.active_levels_id = []
         self.current_quest_index = 0
 
     def getChapterId(self):
         return self.chapter_id
 
-    def getCurrentLevelsId(self):
-        return self.current_levels_id
-
     def getCurrentQuestIndex(self):
         return self.current_quest_index
+
+    def getActiveLevelsId(self):
+        return self.active_levels_id
+
+    def appendActiveLevelsId(self, level_id):
+        if level_id not in self.active_levels_id:
+            self.active_levels_id.append(level_id)
+
+    def clearActiveLevelsId(self):
+        self.active_levels_id = []
 
 
 class PlayerGameData(object):
     def __init__(self):
         self.current_chapter = None
         self._last_level_data = {}
+        self._quest_points = 0
 
     def getCurrentChapterData(self):
         return self.current_chapter
 
-    def loadData(self, active_chapter_id, active_levels_id, active_quest_index):
+    def loadData(self, active_chapter_id, active_levels_id, active_quest_index, quest_points):
         self.current_chapter = ChapterData(active_chapter_id)
-        self.current_chapter.current_levels_id = active_levels_id
         self.current_chapter.current_quest_index = active_quest_index
+        self.current_chapter.active_levels_id = active_levels_id
+        self._quest_points = quest_points
 
     def setLastLevelData(self, value):
         self._last_level_data = value
 
     def getLastLevelData(self):
         return self._last_level_data
+
+    def getQuestPoints(self):
+        return self._quest_points
+
+    def setQuestPoints(self, value):
+        self._quest_points = value
+
+    def incQuestPoints(self, value):
+        self._quest_points += value
--- Resources/Scripts/Database/DatabaseQuests.py
+++ Resources/Scripts/Database/DatabaseQuests.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:56280d51e7086358268abd6d1408b26fb41ef81332fd1106b8eae39a8b0d38f4
-size 997
+oid sha256:3540a86c8dd210c127a35a0aaf1811bd6dca92e53253dab61f844afe50b0c2e0
+size 1123
