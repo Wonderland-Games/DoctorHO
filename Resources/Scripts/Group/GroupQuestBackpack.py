ab0369ae11f3502a8189fe85ad88c2ba95091e64
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2025-04-30 22:42:56 +0300

    add chapter quest items slots movies for ChapterQuestItems

--- Resources/Scripts/Group/GroupQuestBackpack.py
+++ Resources/Scripts/Group/GroupQuestBackpack.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1bb87978ccd3f5836995f8d14df2800a4c3ff0a5cd86582026a652ad843a7d26
-size 827
+oid sha256:19423c3b6d35254cf3d1e149c3726a7262e11365e2d600610a62c5c3716ddb7a
+size 1165
