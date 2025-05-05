1b5b8aa133724c55b7dde80445f11849fab5407d
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2025-05-05 20:18:25 +0300

    add base states logic for QuestItem

--- Game/Scripts/Game/Entities/QuestBackpack/QuestItem.py
+++ Game/Scripts/Game/Entities/QuestBackpack/QuestItem.py
@@ -5,17 +5,25 @@ ITEM_SCALE_MODIFIER = 0.75
 
 
 class QuestItem(Initializer):
+    STATE_BLOCKED = 0
+    STATE_ACTIVE = 1
+
     def __init__(self):
         super(QuestItem, self).__init__()
         self.ref_item_entity = None
         self.item_sprite = None
         self.root = None
+        self.state = None
 
     # - Initializer ----------------------------------------------------------------------------------------------------
 
-    def _onInitialize(self, ref_item_entity):
+    def _onInitialize(self, ref_item_entity, state=STATE_BLOCKED):
         super(QuestItem, self)._onInitialize()
         self.ref_item_entity = ref_item_entity
+
+        self.state = state
+        if self.state is self.STATE_BLOCKED:
+            return
 
         self._createRoot()
         self._setupItemSprite()
@@ -33,6 +41,7 @@ class QuestItem(Initializer):
             self.root = None
 
         self.ref_item_entity = None
+        self.state = None
 
     # - Root -----------------------------------------------------------------------------------------------------------
 
@@ -62,3 +71,11 @@ class QuestItem(Initializer):
         self.item_sprite.setScale((ITEM_SCALE_MODIFIER, ITEM_SCALE_MODIFIER, 1.0))
         self.item_sprite.setLocalPosition(Mengine.vec2f(-item_sprite_center[0] * ITEM_SCALE_MODIFIER,
                                                         -item_sprite_center[1] * ITEM_SCALE_MODIFIER))
+
+    # - State ----------------------------------------------------------------------------------------------------------
+
+    def setState(self, state):
+        self.state = state
+
+    def getState(self):
+        return self.state
