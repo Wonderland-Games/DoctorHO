b195f944819439f8a0d3e1bba7bc948f5f91b496
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2025-05-07 23:14:20 +0300

    WIP QuestItemDescription add name and description texts

--- Game/Scripts/Game/Entities/PopUp/QuestItemDescription.py
+++ Game/Scripts/Game/Entities/PopUp/QuestItemDescription.py
@@ -2,8 +2,19 @@ from UIKit.Entities.PopUp.PopUpContent import PopUpContent
 from Foundation.GroupManager import GroupManager
 
 
-SLOT_ITEM = "Item"
-SLOT_BUTTON = "Button"
+SLOT_ITEM_SPRITE = "ItemSprite"
+
+SLOT_ITEM_NAME = "ItemName"
+SLOT_ITEM_DESCRIPTION = "ItemDescriptionFull"
+
+TEXT_ITEM_NAME_ANNEX = "Name"
+TEXT_ITEM_DESCRIPTION_FULL_ANNEX = "DescriptionFull"
+
+TEXT_ITEM_NAME = "ID_QuestItem_{}".format(TEXT_ITEM_NAME_ANNEX)
+TEXT_ITEM_DESCRIPTION_FULL = "ID_QuestItem_{}".format(TEXT_ITEM_DESCRIPTION_FULL_ANNEX)
+
+TEXT_ITEM_NAME_TEMPLATE = "ID_QuestItem_{}_{}"
+TEXT_ITEM_DESCRIPTION_FULL_TEMPLATE = "ID_QuestItem_{}_{}"
 
 ITEM_BOX_SIZE = 500.0
 
@@ -17,53 +28,57 @@ class QuestItemDescription(PopUpContent):
     def __init__(self):
         super(QuestItemDescription, self).__init__()
 
+        self.item_codename = None
         self.item_sprite = None
-        # self.button = None
-        self.isHoldingItem = False
+        self.item_name = None
+        self.item_description_full = None
 
     # - PopUpContent ---------------------------------------------------------------------------------------------------
 
     def _onInitializeContent(self, **content_args):
         super(QuestItemDescription, self)._onInitializeContent()
 
-        self._setupItem(content_args)
-        # self._setupButton()
-
-        self._adjustSlotsPositions()
-        self._runTaskChains()
-
-    def _onFinalizeContent(self):
-        super(QuestItemDescription, self)._onFinalizeContent()
-
-        if self.item_sprite is not None:
-            if self.isHoldingItem is True:
-                Mengine.destroyNode(self.item_sprite)
-            self.item_sprite = None
-
-        # if self.button is not None:
-        #     self.button.onDestroy()
-        #     self.button = None
-
-    # - Setup ----------------------------------------------------------------------------------------------------------
-
-    def _setupItem(self, content_args):
         chapter_id = content_args["ChapterId"]
         item_name = content_args["ItemName"]
         convert_to_store_item_name = content_args.get("ConvertToStoreItemName", True)
 
         if convert_to_store_item_name is True:
             item_name_raw = item_name.replace("Item_", "")
-            item_name_store = QUEST_ITEM_NAME.format(chapter_id, item_name_raw)
+            self.item_codename = QUEST_ITEM_NAME.format(chapter_id, item_name_raw)
         else:
-            item_name_store = item_name
+            self.item_codename = item_name
 
+        self._setupItemSprite()
+        self._setupItemName()
+        self._setupItemDescriptionFull()
+
+        self._adjustSlotsPositions()
+
+    def _onFinalizeContent(self):
+        super(QuestItemDescription, self)._onFinalizeContent()
+
+        if self.item_description_full is not None:
+            Mengine.destroyNode(self.item_description_full)
+            self.item_description_full = None
+
+        if self.item_name is not None:
+            Mengine.destroyNode(self.item_name)
+            self.item_name = None
+
+        if self.item_sprite is not None:
+            Mengine.destroyNode(self.item_sprite)
+            self.item_sprite = None
+
+        self.item_codename = None
+
+    # - Setup ----------------------------------------------------------------------------------------------------------
+
+    def _setupItemSprite(self):
         item_store_group = GroupManager.getGroup(QUEST_ITEM_STORE_GROUP)
-        item_object = item_store_group.getObject(item_name_store)
+        item_object = item_store_group.getObject(self.item_codename)
         item_entity = item_object.getEntity()
         self.item_sprite = item_entity.generatePure()
         self.item_sprite.enable()
-
-        self.isHoldingItem = True
 
         item_size = self.item_sprite.getSurfaceSize()
         scale_perc_x = ITEM_BOX_SIZE / item_size.x
@@ -71,29 +86,55 @@ class QuestItemDescription(PopUpContent):
         scale_perc = min(scale_perc_x, scale_perc_y)
         self.item_sprite.setScale((scale_perc, scale_perc, 1.0))
 
-        slot = self.content.getMovieSlot(SLOT_ITEM)
+        slot = self.content.getMovieSlot(SLOT_ITEM_SPRITE)
         slot.addChild(self.item_sprite)
 
         item_sprite_center = self.item_sprite.getLocalImageCenter()
         self.item_sprite.setLocalPosition(Mengine.vec2f(-item_sprite_center.x * scale_perc, -item_sprite_center.y * scale_perc))
 
-    def _setupButton(self):
-        self.button = self._generateContainter(SLOT_BUTTON)
-        if self.button is None:
-            return
+    def _setupItemName(self):
+        self.item_name = Mengine.createNode("TextField")
+        self.item_name.setName(self.item_codename + "_" + TEXT_ITEM_NAME_ANNEX)
 
-        self._attachObjectToSlot(self.button, SLOT_BUTTON)
+        self.item_name.setVerticalCenterAlign()
+        self.item_name.setHorizontalCenterAlign()
+
+        self.item_name.setTextId(TEXT_ITEM_NAME)
+        item_name_text_id = TEXT_ITEM_NAME_TEMPLATE.format(self.item_codename, TEXT_ITEM_NAME_ANNEX)
+        item_name_text = Mengine.getTextFromId(item_name_text_id)
+        self.item_name.setTextFormatArgs(item_name_text)
+
+        slot = self.content.getMovieSlot(SLOT_ITEM_NAME)
+        slot.addChild(self.item_name)
+
+        self.item_name.enable()
+
+    def _setupItemDescriptionFull(self):
+        self.item_description_full = Mengine.createNode("TextField")
+        self.item_description_full.setName(self.item_codename + "_" + TEXT_ITEM_DESCRIPTION_FULL_ANNEX)
+
+        self.item_description_full.setVerticalCenterAlign()
+        self.item_description_full.setHorizontalCenterAlign()
+
+        self.item_description_full.setTextId(TEXT_ITEM_DESCRIPTION_FULL)
+        item_description_full_text_id = TEXT_ITEM_DESCRIPTION_FULL_TEMPLATE.format(self.item_codename, TEXT_ITEM_DESCRIPTION_FULL_ANNEX)
+        item_description_full_text = Mengine.getTextFromId(item_description_full_text_id)
+        self.item_description_full.setTextFormatArgs(item_description_full_text)
+
+        slot = self.content.getMovieSlot(SLOT_ITEM_DESCRIPTION)
+        slot.addChild(self.item_description_full)
+
+        self.item_description_full.enable()
 
     def _adjustSlotsPositions(self):
         content_size = self.pop_up_base.getContentSize()
         item_size_raw = self.item_sprite.getSurfaceSize()
         item_scale = self.item_sprite.getScale()
         item_size = Mengine.vec2f(item_size_raw.x * item_scale.x, item_size_raw.y * item_scale.y)
-        # button_size = self.button.getSize()
-        available_size_y = content_size.y - item_size.y
+        available_size_y = content_size.y - item_size.y  # - button_size.y
 
         slots_data = {
-            SLOT_ITEM: item_size.y / 2,
+            SLOT_ITEM_SPRITE: item_size.y / 2,
             # SLOT_BUTTON: button_size.y / 2,
         }
         offset_between_slots = available_size_y / (len(slots_data) + 1)
@@ -108,17 +149,3 @@ class QuestItemDescription(PopUpContent):
             slot.setLocalPosition(Mengine.vec2f(0, current_pos_y))
 
             current_pos_y += obj_half_size
-
-    # - TaskChain ------------------------------------------------------------------------------------------------------
-
-    def _runTaskChains(self):
-        # if self.button is not None:
-        #     with self._createTaskChain(SLOT_BUTTON) as tc:
-        #         tc.addTask("TaskMovie2ButtonClick", Movie2Button=self.button.movie)
-        #         tc.addScope(self._scopeButton)
-        pass
-
-    def _scopeButton(self, source):
-        self.isHoldingItem = False
-
-        source.addNotify(Notificator.onPopUpQuestItemReceived)
