ab9b0661f4f976b1759b1429c4f31481c59bd306
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2025-05-08 21:45:38 +0300

    add modify for popup bg size type

--- Resources/Scripts/Group/GroupUIStore.py
+++ Resources/Scripts/Group/GroupUIStore.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d6bc0139652c6aa10d0371418406d4c1d96f45366c238370aab21c47ca49223
-size 7231
+oid sha256:d19d607b301c081b27f06378db4ac9cdf5a7282c2b952c57718a2f78b25fd1d3
+size 7384
