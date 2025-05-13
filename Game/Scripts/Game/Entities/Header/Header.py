38455fcaa938b4e4dca0ad2abaf4f4d69e0d063e
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2025-05-13 21:44:41 +0300

    add blacklist list for HeaderComponents init

--- Game/Scripts/Game/Entities/Header/Header.py
+++ Game/Scripts/Game/Entities/Header/Header.py
@@ -1,6 +1,12 @@
 from MobileKit.Entities.Header.Header import Header as HeaderBase
 from UIKit.AdjustableScreenUtils import AdjustableScreenUtils
 from Game.Entities.HeaderComponents.Settings import Settings
+from Foundation.SceneManager import SceneManager
+
+
+SETTINGS_BLACKLIST = [
+    "Cutscene",
+]
 
 
 class Header(HeaderBase):
@@ -19,10 +25,14 @@ class Header(HeaderBase):
             ))
 
     def _setup(self):
+        current_scene = SceneManager.getCurrentSceneName()
         header_components = [Settings]
         for component in header_components:
             if self.movie_content.hasSlot(component.slot_name) is False:
                 Trace.log("HeaderComponent", 0, "Not found slot {!r} in {!r}!".format(component.slot_name, self.__class__.__name__))
+                continue
+
+            if current_scene in SETTINGS_BLACKLIST:
                 continue
 
             component_instance = component()
