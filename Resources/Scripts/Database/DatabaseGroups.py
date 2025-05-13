d90b977b2f61f4330520b566491907b22523777c
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2025-05-13 22:39:40 +0300

    add Intro cutscene group

--- Resources/Scripts/Database/DatabaseGroups.py
+++ Resources/Scripts/Database/DatabaseGroups.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:392c02a1ec3e33d0a5dc8f4fd7d37d50260cec3ce11457d62f57b34b466569cc
-size 1316
+oid sha256:5b506f3d4f4e6c0097bc650df7b822293e1b9a248761970c6e8f76e36bde569e
+size 1358
