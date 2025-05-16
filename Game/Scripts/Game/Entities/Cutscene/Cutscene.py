2e02af91645d479a63c104f6eb9c2861f5db0565
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2025-05-16 21:41:20 +0300

    WIP Cutscene - add setup cutscene movie logic

--- Game/Scripts/Game/Entities/Cutscene/Cutscene.py
+++ Game/Scripts/Game/Entities/Cutscene/Cutscene.py
@@ -1,5 +1,8 @@
 from Foundation.Entity.BaseEntity import BaseEntity
 from Foundation.TaskManager import TaskManager
+from Foundation.GroupManager import GroupManager
+from Game.Managers.CutsceneManager import CutsceneManager
+from UIKit.AdjustableScreenUtils import AdjustableScreenUtils
 
 
 MOVIE_CONTENT = "Movie2_Content"
@@ -8,6 +11,11 @@ SLOT_SKIP = "Skip"
 
 
 class Cutscene(BaseEntity):
+    @staticmethod
+    def declareORM(Type):
+        BaseEntity.declareORM(Type)
+        Type.addAction(Type, "CutsceneId")
+
     def __init__(self):
         super(Cutscene, self).__init__()
         self.content = None
@@ -24,6 +32,12 @@ class Cutscene(BaseEntity):
             return
 
         self._setupSkipButton()
+
+        # temp
+        self.object.setParam("CutsceneId", "Intro")
+        if self.CutsceneId is None:
+            return
+
         self._setupCutscene()
 
     def _onActivate(self):
@@ -42,9 +56,7 @@ class Cutscene(BaseEntity):
             self.skip_button.onDestroy()
             self.skip_button = None
 
-        if self.cutscene_movie is not None:
-            self.cutscene_movie.onDestroy()
-            self.cutscene_movie = None
+        self.cutscene_movie = None
 
     # - Setup ----------------------------------------------------------------------------------------------------------
 
@@ -52,7 +64,24 @@ class Cutscene(BaseEntity):
         pass
 
     def _setupCutscene(self):
-        pass
+        cutscene_object = CutsceneManager.getCutscene(self.CutsceneId)
+        if cutscene_object is None:
+            return
+
+        cutscene_group_name = cutscene_object.cutscene_group_name
+        self.cutscene_movie = GroupManager.getObject(cutscene_group_name, cutscene_object.cutscene_movie_name)
+        cutscene_movie_node = self.cutscene_movie.getEntityNode()
+
+        cutscene_slot = self.content.getMovieSlot(SLOT_CUTSCENE)
+        cutscene_slot.addChild(cutscene_movie_node)
+
+        _, _, _, _, _, x_center, y_center = AdjustableScreenUtils.getMainSizesExt()
+        cutscene_slot.setWorldPosition(Mengine.vec2f(x_center, y_center))
+
+        # temp
+        self.cutscene_movie.setEnable(True)
+        self.cutscene_movie.setPlay(True)
+        self.cutscene_movie.setLoop(True)
 
     # - TaskChain ------------------------------------------------------------------------------------------------------
 
--- Game/Scripts/Game/Objects/ObjectCutscene.py
+++ Game/Scripts/Game/Objects/ObjectCutscene.py
@@ -2,4 +2,11 @@ from Foundation.Object.DemonObject import DemonObject
 
 
 class ObjectCutscene(DemonObject):
-    pass
+    @staticmethod
+    def declareORM(Type):
+        DemonObject.declareORM(Type)
+        Type.addParam(Type, "CutsceneId")
+
+    def _onParams(self, params):
+        super(ObjectCutscene, self)._onParams(params)
+        self.initParam("CutsceneId", params, None)
