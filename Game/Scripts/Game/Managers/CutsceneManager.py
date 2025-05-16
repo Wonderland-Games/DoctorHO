d2267fe307655d5d1ae5c0724929344fd3ea5023
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2025-05-16 21:41:20 +0300

    WIP Cutscene - add setup cutscene movie logic

--- Game/Scripts/Game/Managers/CutsceneManager.py
+++ Game/Scripts/Game/Managers/CutsceneManager.py
@@ -7,9 +7,10 @@ class CutsceneManager(Manager):
     _cutscenes = {}
 
     class Cutscene(object):
-        def __init__(self, cutscene_id, cutscene_group_name):
+        def __init__(self, cutscene_id, cutscene_group_name, cutscene_movie_name):
             self.cutscene_id = cutscene_id
             self.cutscene_group_name = cutscene_group_name
+            self.cutscene_movie_name = cutscene_movie_name
 
     @staticmethod
     def loadParams(module, param):
@@ -17,13 +18,23 @@ class CutsceneManager(Manager):
         for record in records:
             cutscene_id = record.get("CutsceneId")
             cutscene_group_name = record.get("CutsceneGroupName")
+            cutscene_movie_name = record.get("CutsceneMovieName")
 
             if GroupManager.hasGroup(cutscene_group_name) is False:
-                Trace.msg_err("CutsceneManager loadParams: invalid add cutscene {!r}, not found group {!r}"
+                Trace.msg_err("CutsceneManager.loadParams: invalid add cutscene {!r}, not found group {!r}"
                               .format(cutscene_id, cutscene_group_name))
                 continue
 
-            cutscene = CutsceneManager.Cutscene(cutscene_id, cutscene_group_name)
+            cutscene = CutsceneManager.Cutscene(cutscene_id, cutscene_group_name, cutscene_movie_name)
             CutsceneManager._cutscenes[cutscene_id] = cutscene
 
         return True
+
+    @staticmethod
+    def getCutscene(cutscene_id):
+        cutscene = CutsceneManager._cutscenes.get(cutscene_id)
+        if cutscene is None:
+            Trace.msg_err("CutsceneManager.getCutscene: invalid cutscene id {!r}".format(cutscene_id))
+            return None
+
+        return cutscene
