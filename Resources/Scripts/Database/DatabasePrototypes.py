7c15052b0277ae197f36bb68873103dd7ba5dc6c
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2025-05-26 22:50:05 +0300

    WIP Cutscene

--- Resources/Scripts/Database/DatabasePrototypes.py
+++ Resources/Scripts/Database/DatabasePrototypes.py
@@ -34,6 +34,7 @@ class DatabasePrototypes(Database):
         self.addORM(RecordPrototypes("LevelLost_Ad", None, None, "Movie2Button_Rectangle_Green", "Movie2Button_Rectangle_Green", None, Icon=dict(Slot="icon", Prototype="Advertising", Size="Medium")))
         self.addORM(RecordPrototypes("LevelLost_Restart", None, None, "Movie2Button_Rectangle", "Movie2Button_Rectangle", None, {}))
         self.addORM(RecordPrototypes("LevelLost_Lobby", None, None, "Movie2Button_Rectangle", "Movie2Button_Rectangle", None, {}))
+        self.addORM(RecordPrototypes("Languages_Buttons", None, None, "Movie2Button_Rectangle", "Movie2Button_Rectangle", None, {}))
         self.addORM(RecordPrototypes("LevelWon_Lobby", None, None, "Movie2Button_Rectangle", "Movie2Button_Rectangle", None, Icon=dict(Slot="icon", Prototype="Lobby")))
         self.addORM(RecordPrototypes("Lobby_Play", None, None, "Movie2Button_Rectangle_Green", "Movie2Button_Rectangle_Green", None, {}))
         self.addORM(RecordPrototypes("MissClickEffect", None, None, "Movie2_MissClickEffect", "Movie2_MissClickEffect", None, {}))
@@ -45,5 +46,6 @@ class DatabasePrototypes(Database):
         self.addORM(RecordPrototypes("QuestItemReceived_Button", None, None, "Movie2Button_Rectangle_Green", "Movie2Button_Rectangle_Green", None, {}))
         self.addORM(RecordPrototypes("QuestBackpack", None, None, "Movie2Button_Square_256", "Movie2Button_Square_256", None, Icon=dict(Slot="icon", Prototype="Backpack")))
         self.addORM(RecordPrototypes("Lobby", None, None, "Movie2Button_Square_256", "Movie2Button_Square_256", None, Icon=dict(Slot="icon", Prototype="InvestigationBoard")))
+        self.addORM(RecordPrototypes("Cutscene_Skip", None, None, "Movie2Button_Rectangle_Green", "Movie2Button_Rectangle_Green", None, {}))
         pass
     pass
