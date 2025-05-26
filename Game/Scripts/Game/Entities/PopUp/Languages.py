dee269117ef78ee6f2cd7faf5e0e9f42d2067660
Constantin <pshenyanik@gmail.com>
2025-05-26 20:25:23 +0300

    Update laguage popup

--- Game/Scripts/Game/Entities/PopUp/Languages.py
+++ Game/Scripts/Game/Entities/PopUp/Languages.py
@@ -1,16 +1,11 @@
 from UIKit.Entities.PopUp.PopUpContent import PopUpContent
 from UIKit.Managers.PrototypeManager import PrototypeManager
 from Foundation.LanguagesManager import LanguagesManager
-from Foundation.SystemManager import SystemManager
-from Foundation.DefaultManager import DefaultManager
 from Foundation.TaskManager import TaskManager
 
 
 SLOT_BUTTONS = "Buttons"
 ALIAS_BUTTON = "$UIButton"
-
-LANG_OFFSET_X = DefaultManager.getDefaultFloat("LanguagesOffsetX", 270.0)
-LANG_OFFSET_Y = DefaultManager.getDefaultFloat("LanguagesOffsetY", 180.0)
 
 
 class Languages(PopUpContent):
@@ -28,7 +23,6 @@ class Languages(PopUpContent):
 
         self._setupButtons()
         self._setupSlotsPositions()
-        #self._setButtonsPosition_2()
 
         self._runTaskChains()
 
@@ -43,12 +37,9 @@ class Languages(PopUpContent):
     # - Setup ----------------------------------------------------------------------------------------------------------
 
     def _setupButtons(self):
-        #self._changeLocale(locale)
         locales = LanguagesManager.getLocales()
-        print(locales)
 
         for name in locales:
-
             container = self._generateContainer(name)
             if container is None:
                 continue
@@ -61,7 +52,7 @@ class Languages(PopUpContent):
         for (key, button) in self.buttons_list.items():
             objects_list.append({key: button})
 
-        self._setButtonsPosition(objects_list)
+        self.setupObjectsSlotsAsTable(objects_list, False)
 
     # - TaskChain ------------------------------------------------------------------------------------------------------
 
@@ -95,67 +86,6 @@ class Languages(PopUpContent):
 
         return container
 
-
-    def _setButtonsPosition(self, objects_list):
-        # prepare variables
-        content_size = self.pop_up_base.getContentSize()
-        start_pos = Mengine.vec2f(0, 0)
-        start_pos -= content_size / 2
-
-        # setup objects between each other
-        current_size_y = 0
-        objects_len_y = len(objects_list)
-        for row in objects_list:
-
-            current_size_x = 0
-            maximum_size_y = 0
-
-            for key, obj in row.items():
-                obj_pos = obj.getLocalPosition()
-                obj_pos += start_pos
-
-                obj_size = obj.getSize()
-                obj_pos += obj_size / 2
-
-                obj_pos.x += current_size_x
-                obj_pos.y += current_size_y
-
-                obj_slot = obj.getEntityNode()
-                obj_slot.setLocalPosition(obj_pos)
-
-                current_size_x += obj_size.x
-                if maximum_size_y < obj_size.y:
-                    maximum_size_y = obj_size.y
-
-            current_size_y += maximum_size_y
-
-        # calc offset y
-        available_space_y = content_size.y - current_size_y
-        offset_y = available_space_y / (objects_len_y + 1)
-
-        # adding offsets between objects
-        for y, row in enumerate(objects_list):
-            current_size_x = 0
-            objects_len_x = len(row.items())
-
-            # add offset y
-            for key, obj in row.items():
-                old_pos = obj.getLocalPosition()
-                new_pos = Mengine.vec2f(old_pos.x, old_pos.y + offset_y * (y + 1))
-                obj.setLocalPosition(new_pos)
-
-                obj_size = obj.getSize()
-                current_size_x += obj_size.x
-
-            # calc offset x
-            available_space_x = content_size.x - current_size_x
-            offset_x = available_space_x / (objects_len_x + 1)
-
-            # add offset x
-            for x, (key, obj) in enumerate(row.items()):
-                old_pos = obj.getLocalPosition()
-                new_pos = Mengine.vec2f(old_pos.x + offset_x * (x + 1), old_pos.y)
-                obj.setLocalPosition(new_pos)
 
     def _changeLocale(self, locale):
         if LanguagesManager.getLocale() == locale:
