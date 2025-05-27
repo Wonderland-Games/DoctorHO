c7dc292cec2215788b3dec6cb21e629c2e84b8c9
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2025-05-27 22:11:45 +0300

    WIP Cutscene

--- Resources/Scripts/Database/DatabaseCutscenes.py
+++ Resources/Scripts/Database/DatabaseCutscenes.py
@@ -3,6 +3,6 @@ from Foundation.Database import Database
 class DatabaseCutscenes(Database):
     def __init__(self):
         super(DatabaseCutscenes, self).__init__()
-        self.addRecord(CutsceneId="Intro", CutsceneGroupName="Intro", CutsceneMovieName="Movie2_Cutscene", CutsceneMovies=["Movie2_Play_1", "Movie2_Loop_1", "Movie2_Play_2", "Movie2_Loop_2"])
+        self.addRecord(CutsceneId="Intro", CutsceneGroupName="Intro", CutsceneMovies=["Movie2_Play_1", "Movie2_Loop_1", "Movie2_Play_2", "Movie2_Loop_2"])
         pass
     pass
