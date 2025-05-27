ddd0a74ceab557d538c5d5a620ffe43c4e5236a6
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2025-05-27 22:11:45 +0300

    WIP Cutscene

--- Resources/Scripts/Group/GroupIntro.py
+++ Resources/Scripts/Group/GroupIntro.py
@@ -10,7 +10,6 @@ class GroupIntro(Group):
 
     def _onLoader(self):
         self.createLayer( "Layer2D_Main", Type = "Layer2D", Size = (2736, 1536), Main = True )
-        self.createObject( "Movie2", Name = "Movie2_Cutscene" , ResourceMovie = "Movie2_Intro", CompositionName = "Cutscene"  )
         self.createObject( "Movie2", Name = "Movie2_Play_1" , ResourceMovie = "Movie2_Intro", CompositionName = "Play_1"  )
         self.createObject( "Movie2", Name = "Movie2_Loop_1" , ResourceMovie = "Movie2_Intro", CompositionName = "Loop_1"  )
         self.createObject( "Movie2", Name = "Movie2_Play_2" , ResourceMovie = "Movie2_Intro", CompositionName = "Play_2"  )
