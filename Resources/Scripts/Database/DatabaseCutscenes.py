e7a6ddf2cf9325be9f1221976a537ad1f5330885
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2025-05-30 21:44:30 +0300

    excel export

--- Resources/Scripts/Database/DatabaseCutscenes.py
+++ Resources/Scripts/Database/DatabaseCutscenes.py
@@ -4,6 +4,5 @@ class DatabaseCutscenes(Database):
     def __init__(self):
         super(DatabaseCutscenes, self).__init__()
         self.addRecord(CutsceneId="Intro", CutsceneGroupName="Intro", CutsceneMovies=[])
-        self.addRecord(CutsceneMovies=["Movie2_Play_1", "Movie2_Loop_1", "Movie2_Play_2", "Movie2_Loop_2"])
         pass
     pass
