05474185f21adf63e340a2f9bdd035dc8b2bcf0e
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2025-07-10 16:25:14 +0300

    move layout wrappers objects

--- Game/Scripts/Game/Entities/PopUp/QuestItemDescription.py
+++ Game/Scripts/Game/Entities/PopUp/QuestItemDescription.py
@@ -1,4 +1,5 @@
 from UIKit.Entities.PopUp.PopUpContent import PopUpContent, LayoutBox
+from UIKit.LayoutWrapper import LayoutWrapper
 from Foundation.GroupManager import GroupManager
 
 
@@ -187,49 +188,8 @@ class QuestItemDescription(PopUpContent):
             current_pos_y += obj_half_size
 
     def _setupLayoutBox(self):
-        class SpriteWrapper:
-            def __init__(self, sprite):
-                self.sprite = sprite
-
-            def getSpriteSize(self):
-                sprite_size_unscaled = self.sprite.getSurfaceSize()
-                sprite_scale = self.sprite.getScale()
-                sprite_size = (sprite_size_unscaled.x * sprite_scale.x, sprite_size_unscaled.y * sprite_scale.y)
-                return sprite_size
-
-            def getLayoutSize(self):
-                return self.getSpriteSize()
-
-            def setLayoutOffset(self, layout_box, layout_offset, layout_size):
-                layout_box_width, layout_box_height = layout_box.getSize()
-                sprite_size = self.getSpriteSize()
-                self.sprite.setLocalPosition((
-                    layout_offset[0] + layout_size[0] / 2 - layout_box_width / 2 - sprite_size[0] / 2,
-                    layout_offset[1] + layout_size[1] / 2 - layout_box_height / 2 - sprite_size[1] / 2
-                ))
-
-        item_sprite_wrapper = SpriteWrapper(self.item_sprite)
-
-        class TextWrapper:
-            def __init__(self, text):
-                self.text = text
-
-            def getTextSize(self):
-                # text_size = self.text.calcTotalTextSize()
-                text_size = self.text.getTextSize()
-                return (text_size.x, text_size.y)
-
-            def getLayoutSize(self):
-                return self.getTextSize()
-
-            def setLayoutOffset(self, layout_box, layout_offset, layout_size):
-                layout_box_width, layout_box_height = layout_box.getSize()
-                self.text.setLocalPosition((
-                    layout_offset[0] + layout_size[0] / 2 - layout_box_width / 2,
-                    layout_offset[1] + layout_size[1] / 2 - layout_box_height / 2
-                ))
-
-        item_description_full_wrapper = TextWrapper(self.item_description_full)
+        item_sprite_wrapper = LayoutWrapper.SpriteWrapper(self.item_sprite)
+        item_description_full_wrapper = LayoutWrapper.TextWrapper(self.item_description_full)
 
         with LayoutBox.BuilderVertical(self.layout_box) as vertical:
             vertical.addPadding(1)
--- Game/Scripts/Game/Entities/PopUp/QuestItemReceived.py
+++ Game/Scripts/Game/Entities/PopUp/QuestItemReceived.py
@@ -1,4 +1,5 @@
 from UIKit.Entities.PopUp.PopUpContent import PopUpContent, LayoutBox
+from UIKit.LayoutWrapper import LayoutWrapper
 from Foundation.GroupManager import GroupManager
 
 
@@ -105,28 +106,7 @@ class QuestItemReceived(PopUpContent):
             current_pos_y += obj_half_size
 
     def _setupLayoutBox(self):
-        class SpriteWrapper:
-            def __init__(self, sprite):
-                self.sprite = sprite
-
-            def getSpriteSize(self):
-                sprite_size_unscaled = self.sprite.getSurfaceSize()
-                sprite_scale = self.sprite.getScale()
-                sprite_size = (sprite_size_unscaled.x * sprite_scale.x, sprite_size_unscaled.y * sprite_scale.y)
-                return sprite_size
-
-            def getLayoutSize(self):
-                return self.getSpriteSize()
-
-            def setLayoutOffset(self, layout_box, layout_offset, layout_size):
-                layout_box_width, layout_box_height = layout_box.getSize()
-                sprite_size = self.getSpriteSize()
-                self.sprite.setLocalPosition((
-                    layout_offset[0] + layout_size[0] / 2 - layout_box_width / 2 - sprite_size[0] / 2,
-                    layout_offset[1] + layout_size[1] / 2 - layout_box_height / 2 - sprite_size[1] / 2
-                ))
-
-        item_sprite_wrapper = SpriteWrapper(self.item_sprite)
+        item_sprite_wrapper = LayoutWrapper.SpriteWrapper(self.item_sprite)
 
         with LayoutBox.BuilderVertical(self.layout_box) as vertical:
             vertical.addPadding(1)
