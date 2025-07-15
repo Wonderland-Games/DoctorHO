7c7392b15c9ff0fc58a3b939f939468264308b82
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2025-07-15 18:38:53 +0300

    Squashed commit of the following:
    commit 59849869e9cc26f93224dc0f0f4e3cbff9c38e8c
    Author: Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
    Date:   Tue Jul 15 16:36:27 2025 +0300
    
        rework
    
    commit 5a02a7781a9297650699fbe989c631901eff5ac4
    Author: Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
    Date:   Tue Jul 15 16:05:29 2025 +0300
    
        rename banner into dummy banner
    
    commit 461504287cf87cc69082229a9d5217a8bd259b83
    Author: Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
    Date:   Thu Jun 26 22:29:01 2025 +0300
    
        refactor dummy banner code
    
    commit 1304aeaaf30351d6264e141091649478cb523842
    Author: Constantin <pshenyanik@gmail.com>
    Date:   Tue Jun 24 19:59:55 2025 +0300
    
        Change set banner size
    
    commit d23f142167c87be0ab6ca3856c65f936ea7c2163
    Author: Constantin <pshenyanik@gmail.com>
    Date:   Tue Jun 24 08:35:16 2025 +0300
    
        Update setup banner for PC
    
    commit 50d22dbb80fb3d2828ffff1ba89013b3f196b020
    Author: Constantin <pshenyanik@gmail.com>
    Date:   Mon Jun 23 17:58:04 2025 +0300
    
        Update devAdjusterBanner
    
    commit 20619a841641aec8af1b4b30de60d31027498fd6
    Author: Constantin <pshenyanik@gmail.com>
    Date:   Wed Jun 18 21:31:00 2025 +0300
    
        Not scale banner height

--- Resources/Scripts/Database/DatabaseSceneSlots_QuestBackpack.py
+++ Resources/Scripts/Database/DatabaseSceneSlots_QuestBackpack.py
@@ -7,7 +7,7 @@ class DatabaseSceneSlots_QuestBackpack(Database):
         self.addRecord(Name="QuestBackpack", Type="Layer2D", Width=2736, Height=1536, Main=1)
         self.addRecord(Name="QuestItemStore", Type="Layer2D", Width=2736, Height=1536, Main=0)
         self.addRecord(Name="Header", Type="Layer2D", Width=2736, Height=1536, Main=0)
-        self.addRecord(Name="Banner", Type="Layer2D", Width=2736, Height=1536, Main=0, Platform="PC")
+        self.addRecord(Name="DummyBanner", Type="Layer2D", Width=2736, Height=1536, Main=0, Platform="PC")
         self.addRecord(Name="FadeUI", Type="Layer2D", Width=2736, Height=1536, Main=0)
         self.addRecord(Name="PopUp", Type="Layer2D", Width=2736, Height=1536, Main=0)
         self.addRecord(Name="Fade", Type="Layer2D", Width=2736, Height=1536, Main=0)
