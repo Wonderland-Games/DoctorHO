590dc160cbe8e2646a563262506ada23a8accf43
Constantin <pshenyanik@gmail.com>
2025-07-17 21:57:49 +0300

    Second iteration

--- Resources/Scripts/Database/DatabaseScenes.py
+++ Resources/Scripts/Database/DatabaseScenes.py
@@ -6,6 +6,7 @@ class DatabaseScenes(Database):
         self.addRecord(SceneName="Loading", DefaultScene="Loading", BaseScene="Loading", SlotName="Loading", SceneType="Scene", GroupName="Loading", GameScene=0)
         self.addRecord(SceneName="Lobby", DefaultScene="Lobby", BaseScene="Lobby", SlotName="Lobby", SceneType="Scene", GroupName="Lobby", GameScene=0)
         self.addRecord(SceneName="GameArea", DefaultScene="GameArea", BaseScene="GameArea", SlotName="GameArea", SceneType="Scene", GroupName="GameArea", GameScene=0)
+        self.addRecord(SceneName="FinalStage", DefaultScene="FinalStage", BaseScene="FinalStage", SlotName="FinalStage", SceneType="Scene", GroupName="FinalStage", GameScene=0)
         self.addRecord(SceneName="QuestBackpack", DefaultScene="QuestBackpack", BaseScene="QuestBackpack", SlotName="QuestBackpack", SceneType="Scene", GroupName="QuestBackpack", GameScene=0)
         self.addRecord(SceneName="Cutscene", DefaultScene="Cutscene", BaseScene="Cutscene", SlotName="Cutscene", SceneType="Scene", GroupName="Cutscene", GameScene=0)
         pass
