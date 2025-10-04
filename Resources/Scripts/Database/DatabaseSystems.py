46b9c52f04a5f01940b4ddcc9a9ac3fa18186693
yurii.levchenko <yurii.levchenko@murka.com>
2025-10-04 18:14:33 +0300

    Wip

--- Resources/Scripts/Database/DatabaseSystems.py
+++ Resources/Scripts/Database/DatabaseSystems.py
@@ -3,12 +3,20 @@ from Foundation.Database import Database
 class DatabaseSystems(Database):
     def __init__(self):
         super(DatabaseSystems, self).__init__()
-        self.addRecord(Module="Game.Systems", Name="SystemGlobal", Global=1, Priority=1)
-        self.addRecord(Module="Game.Systems", Name="SystemGame", Global=1)
+        self.addRecord(Module="Foundation.Systems", Name="SystemAutoSave", Global=1)
         self.addRecord(Module="Foundation.Systems", Name="SystemDebugNotifications", Global=1, Development=1)
+        self.addRecord(Module="Foundation.Systems", Name="SystemGoogleServices", Global=1)
+        self.addRecord(Module="Foundation.Systems", Name="SystemAppleServices", Global=1)
+        self.addRecord(Module="Foundation.Systems", Name="SystemApplovin", Global=1)
+        self.addRecord(Module="Foundation.Systems", Name="SystemFacebook", Global=1)
+        self.addRecord(Module="Foundation.Systems", Name="SystemRemoteConfig", Global=1)
+        self.addRecord(Module="Foundation.Systems", Name="SystemMonetization", Global=1, Platform="MOBILE", Priority=1)
+        self.addRecord(Module="Foundation.Systems", Name="SystemAdvertising", Global=1)
         self.addRecord(Module="Foundation.Systems", Name="SystemFade", Global=1)
+        self.addRecord(Module="HOPA.System", Name="SystemAnalytics", Global=1)
         self.addRecord(Module="UIKit.Systems", Name="SystemPopUp", Global=1)
         self.addRecord(Module="Game.Systems", Name="SystemUI", Global=1)
-        self.addRecord(Module="Foundation.Systems", Name="SystemAdvertising", Global=1)
+        self.addRecord(Module="Game.Systems", Name="SystemGlobal", Global=1, Priority=1)
+        self.addRecord(Module="Game.Systems", Name="SystemGame", Global=1)
         pass
     pass
