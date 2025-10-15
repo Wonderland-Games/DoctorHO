1b51820fe35e38f11eb8bec8a22248ef348ec7b0
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2025-10-15 11:54:08 +0300

    export db default

--- Resources/Scripts/Database/DatabaseDefault.py
+++ Resources/Scripts/Database/DatabaseDefault.py
@@ -9,5 +9,8 @@ class DatabaseDefault(Database):
         self.addRecord(Param="DefaultMobileArrowRadius", Debug=15.0, Master=15.0)
         self.addRecord(Param="TransitionFadeInTime", Debug=0.25, Master=0.25)
         self.addRecord(Param="TransitionFadeOutTime", Debug=0.25, Master=0.25)
+        self.addRecord(Param="AutoSaveonWillTerminate", Debug=True, Master=True)
+        self.addRecord(Param="AutoSaveWillResignActive", Debug=True, Master=True)
+        self.addRecord(Param="AutoSaveTransition", Debug=True, Master=True)
         pass
     pass
